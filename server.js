f215bcb77a79a4e96105a4969325a5aeb4ab5d5b
pvczku <patryk.kulma.dev@gmail.com>
2023-03-01 16:44:09 +0000

    initial app config

--- server.js
+++ server.js
@@ -5,3 +5,9 @@ const http = require('http');
 const server = http.createServer(app);
 const {Server} = require('socket.io');
 const socketio = new Server(server);
+
+app.use(express.static('static'));
+
+app.get('/', (req, res) => res.render("index.html"));
+
+server.listen(PORT, () => console.log(`server works at ${PORT}`));
