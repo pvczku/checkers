fcf6d0fd337fe297dd1acbd2f7637acb3db15094
pvczku <patryk.kulma.dev@gmail.com>
2023-03-01 19:44:40 +0000

    done login

--- server.js
+++ server.js
@@ -1,14 +1,50 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3000;
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
-const {Server} = require('socket.io');
+const { Server } = require("socket.io");
 const socketio = new Server(server);
 
-app.use(express.json());
-app.use(express.static('static'));
+let users = [];
+let theSame = false;
 
-app.get('/', (req, res) => res.render("./index.html"));
+app.use(express.json());
+app.use(express.static("static"));
+
+app.get("/", (req, res) => {
+  res.render("./index.html");
+  console.log(users);
+});
+
+app.post("/ADD_USER", (req, res) => {
+  theSame = false;
+  console.log(users);
+  if (users.length === 2) {
+    res.send({ response: "full", users: users });
+    return;
+  } else {
+    if (users.length > 0) {
+      for (let i = 0; i < users.length; i++) {
+        if (users[i] === req.body.user) {
+          theSame = true;
+        }
+      }
+    }
+    if (theSame === false) {
+      users.push(req.body.user);
+      res.send({ response: "ok", users: users });
+      return;
+    } else {
+      res.send({ response: "error", users: users });
+      return;
+    }
+  }
+});
+
+app.post("/RESET", (req, res) => {
+  users = [];
+  res.send({ response: "ok" });
+});
 
 server.listen(PORT, () => console.log(`server works at ${PORT}`));
--- static/js/Ui.js
+++ static/js/Ui.js
@@ -1,5 +1,64 @@
 export default class Ui {
-    constructor() {
-        
-    }
+  constructor() {}
+
+  createLogin = () => {
+    let loginWrapper = document.createElement("div");
+    loginWrapper.id = "loginWrapper";
+    loginWrapper.className = "loginWrapper";
+    let loginBackground = document.createElement("div");
+    loginBackground.className = "loginBackground";
+    loginBackground.id = "loginBackground";
+    let nicknameInput = document.createElement("input");
+    nicknameInput.id = "nicknameInput";
+    nicknameInput.className = "nicknameInput";
+    let loginButton = document.createElement("button");
+    loginButton.id = "loginButton";
+    loginButton.className = "loginButton";
+    loginButton.innerHTML = "Login";
+    loginButton.onclick = () => {
+      let nickname = document.getElementById("nicknameInput").value;
+      if (nickname.length > 0) {
+        const body = JSON.stringify({ user: nickname }); // body czyli przesyłane na serwer dane
+
+        const headers = { "Content-Type": "application/json" }; // nagłowek czyli typ danych
+
+        fetch("/ADD_USER", { method: "post", body, headers }) // fetch
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.response === "error") {
+              nicknameInput.value = "";
+              alert("Person with this nickname already exists");
+            } else if (data.response === "ok") {
+              this.closeLogin();
+            } else if (data.response === "full") {
+              alert("The game is full");
+            }
+          });
+      } else {
+        alert("Please enter a nickname");
+      }
+    };
+    let resetButton = document.createElement("button");
+    resetButton.id = "resetButton";
+    resetButton.className = "resetButton";
+    resetButton.innerHTML = "Reset";
+    resetButton.onclick = () => {
+      const body = JSON.stringify({ reset: true }); // body czyli przesyłane na serwer dane
+
+      const headers = { "Content-Type": "application/json" }; // nagłowek czyli typ danych
+
+      fetch("/RESET", { method: "post", body, headers }) // fetch
+        .then((response) => response.json())
+        .then((data) => {});
+    };
+    loginBackground.appendChild(nicknameInput);
+    loginBackground.appendChild(loginButton);
+    loginBackground.appendChild(resetButton);
+    loginWrapper.appendChild(loginBackground);
+    document.body.appendChild(loginWrapper);
+  };
+
+  closeLogin = () => {
+    document.getElementById("loginWrapper").remove();
+  };
 }
