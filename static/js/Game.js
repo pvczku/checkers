933d9cd32225b594a7503d02b3c79054ddf9ab5a
pvczku <patryk.kulma.dev@gmail.com>
2023-03-01 17:06:44 +0000

    init

--- static/js/Game.js
+++ static/js/Game.js
@@ -1,21 +1,24 @@
 export default class Game {
 
-        constructor() {
+    constructor() {
     
-            this.scene = new THREE.Scene();
-            this.camera = new THREE.PerspectiveCamera(45, 4 / 3, 0.1, 10000  );
-            this.renderer = new THREE.WebGLRenderer();
-            this.renderer.setClearColor(0x0066ff);
-            this.renderer.setSize(600, 600);
-            document.getElementById("root").append(this.renderer.domElement);
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(45, 4 / 3, 0.1, 10000  );
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setClearColor(0x0066ff);
+        this.renderer.setSize(600, 600);
+        document.getElementById("root").append(this.renderer.domElement);
     
-            this.render() // wywołanie metody render
+        this.camera.position.set(100,100,100)
+        this.camera.lookAt(this.scene.position)
+
+        this.render() // wywołanie metody render
     
-        }
+        }
     
-        render = () => {
-            requestAnimationFrame(this.render);
-            this.renderer.render(this.scene, this.camera);
-            console.log("render leci")
-        }
+        render = () => {
+            requestAnimationFrame(this.render);
+            this.renderer.render(this.scene, this.camera);
+            console.log("render leci")
+        }
     }
